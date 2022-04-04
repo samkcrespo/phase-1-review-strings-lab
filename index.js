d93c76934eed155e62b85a5ceeaef15f1e37d489
Samantha Crespo <samkcrespo@gmail.com>
2022-04-04 10:42:13 -0700

    commit tree 1b79ebfe856d5258c55e138a463a2c6ae244ed4f

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} + !`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase ();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
 // Write your code in this file!
